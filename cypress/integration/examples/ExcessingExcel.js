f3a832b3e357323f95649c5f19cb670d48c37fcb
Arun Purohit <rajpurohitarun97@gmail.com>
2024-10-27 15:39:55 +0530

    working on ExcessingExcel.js

--- cypress/integration/examples/ExcessingExcel.js
+++ cypress/integration/examples/ExcessingExcel.js
@@ -1,5 +1,20 @@
 const ExcelJs = require('exceljs')
 
 const workbook = new ExcelJs.Workbook()
-workbook.getWorksheet('Sheet1')
+workbook.xlsx.readFile("C:/Users/arun9/Downloads/fruits.xlsx").then(() => {
+    const currentWorkSheet = workbook.getWorksheet('Sheet1')
+    currentWorkSheet.eachRow((row, rowNumber) => {
+        row.eachCell((cell, columnNumber) => {
+            //console.log(cell.value)
+            if (cell.value == "Apple") {
+                
+                
+                
+                
+            }
+        })
+    })
+})
+
+
 
